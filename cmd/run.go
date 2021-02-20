a8a44bf4ce297bcf27c60c13310a71282cb8da76
Brandon Bloom <brandon@brandonbloom.name>
2021-02-19 17:09:26 -0800

    cleanup flags usage for run command

--- cmd/run.go
+++ cmd/run.go
@@ -10,12 +10,13 @@ func init() {
 }
 
 var runCmd = &cobra.Command{
-	Use:   "run <script> [args...]",
+	Use:   "run [flags] <script> [args...]",
 	Short: "Build and run an entrypoint.",
 	Long: `Builds and runs the given entrypoint file.
 The script must export a function called "main", which will receive the
 given string args.`,
-	Args: cobra.MinimumNArgs(1),
+	Args:                  cobra.MinimumNArgs(1),
+	DisableFlagsInUseLine: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		repo := mustLoadRepository()
 		return internal.Run(repo, args[0], args[1:])
