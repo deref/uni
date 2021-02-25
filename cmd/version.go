b84a03b00fc879645011c2e8805b1778e3b07168
Brandon Bloom <brandon@brandonbloom.name>
2021-02-25 11:23:26 -0800

    add version command

---
+++ cmd/version.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"github.com/spf13/cobra"
+)
+
+var verbose bool
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+	versionCmd.Flags().BoolVar(&verbose, "verbose", false, "Prints version information for all dependencies too.")
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version of Unirepo.",
+	Long:  "Print the version of Unirepo.",
+	Run: func(cmd *cobra.Command, args []string) {
+		buildInfo, ok := debug.ReadBuildInfo()
+		if !ok {
+			panic("debug.ReadBuildInfo() failed")
+		}
+		printInfo := func(mod debug.Module) {
+			if verbose {
+				fmt.Println(mod.Path, mod.Version)
+			} else {
+				fmt.Println(mod.Version)
+			}
+		}
+		printInfo(buildInfo.Main)
+		if verbose {
+			for _, dep := range buildInfo.Deps {
+				fmt.Println(dep.Path, dep.Version)
+			}
+		}
+	},
+}
