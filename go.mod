b249e55058db572cca8eb917ed5a8054adaeaed4
Brandon Bloom <brandon@brandonbloom.name>
2021-03-10 08:19:30 -0800

    write json files atomically

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/evanw/esbuild v0.8.49
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/goccy/go-yaml v1.8.8
+	github.com/natefinch/atomic v0.0.0-20200526193002-18c0533a5b09
 	github.com/spf13/cobra v1.1.3
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/sys v0.0.0-20210228012217-479acdf4ea46 // indirect
--- internal/json.go
+++ internal/json.go
@@ -3,8 +3,12 @@ package internal
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path"
+
+	"github.com/natefinch/atomic"
+	"golang.org/x/sync/errgroup"
 )
 
 func ReadJSON(filename string, data interface{}) (err error) {
@@ -28,19 +32,20 @@ func WriteJSON(filename string, data interface{}) (err error) {
 	if err := os.MkdirAll(path.Dir(filename), 0755); err != nil {
 		return err
 	}
-	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err1 := f.Close(); err == nil {
-			err = err1
-		}
-	}()
-	enc := json.NewEncoder(f)
-	enc.SetIndent("", "  ")
-	if err := enc.Encode(data); err != nil {
-		return err
-	}
-	return
+
+	r, w := io.Pipe()
+
+	var eg errgroup.Group
+	eg.Go(func() error {
+		defer w.Close()
+		enc := json.NewEncoder(w)
+		enc.SetIndent("", "  ")
+		return enc.Encode(data)
+	})
+
+	eg.Go(func() error {
+		return atomic.WriteFile(filename, r)
+	})
+
+	return eg.Wait()
 }
