a9c28361b6ab5c5a1b08300d8a336e1bf25b377c
Brandon Bloom <brandon@brandonbloom.name>
2021-04-29 09:57:44 -0700

    add "@types/source-map-support" as automatic dependency

--- internal/required.go
+++ internal/required.go
@@ -1,6 +1,7 @@
 package internal
 
 var requiredDependencies = map[string]string{
-	"source-map-support": "0.5.19",
-	"patch-package":      "6.2.2",
+	"source-map-support":        "0.5.19",
+	"@types/source-map-support": "0.5.3",
+	"patch-package":             "6.2.2",
 }
